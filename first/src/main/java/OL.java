7aabd3a0d2dca42e0c1601c060cdfcc74650a651
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-08-21 22:49:57 -0400

    Add files via upload

---
+++ first/src/main/java/OL.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class OL
+{
+    public static void main(String[] args) {
+        System.out.println("hola");
+        for(int i=0;i<40;i++){
+            System.out.println("hola "+i);
+        }
+    }
+}
