05bce38f2775d4ef0310b63e198018e7f526d873
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-08-21 23:27:19 -0400

    Add files via upload

---
+++ wii/src/Hi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hi {
+    public static int sumEdad(int edad){
+        return edad+20;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hellow wordl ");
+        System.out.println("Mi name is marco");
+        float a=sumEdad(40);
+        System.out.println(a);
+        int j=20;
+    }
+}
