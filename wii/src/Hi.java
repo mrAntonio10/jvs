bde4fde443e4ea4ae8d871ec4c8fb64e53b41ddc
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-08-21 23:26:43 -0400

    Delete wii directory

--- wii/src/Hi.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Hi {
-    public static void main(String[] args) {
-        System.out.println("Hellow wordl ");
-        System.out.println("Mi name is marco");
-        int a=20;
-    }
-}
