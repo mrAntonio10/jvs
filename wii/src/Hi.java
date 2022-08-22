adab9b8a25ad49841f968450d3c3dbbb771451b7
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-08-21 23:17:07 -0400

    Add files via upload

---
+++ wii/src/Hi.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hellow wordl ");
+        System.out.println("Mi name is marco");
+        int a=20;
+    }
+}
